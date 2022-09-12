93b79db817d7c8ddc77254c424f806b816cc6ae9
Huckiel <huckielsukai7@gmail.com>
2022-09-12 03:04:51 -0700

    factors.c

--- factors.c
+++ factors.c
@@ -1,145 +1,96 @@
-#include "rsa.h"
-#include "pollard-rho.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gmp.h>
 
 /**
- * init - Initializes the text buffer with null char
- *
- * @buffer: Array that will hold numbers
- *
- * Return: None (void function)
- */
-void init(char *buffer)
+  * factor - factorization handler function
+  * @p: integer pointer
+  * @n: integer
+  *
+  * Return: void
+  */
+void factor(mpz_t *p, mpz_t n)
 {
-	int i = 0;
+	mpz_t r;
+	
+	mpz_set_ui(*p, 2U);
 
-	for (i = 0; i < S_BUFFER; i++)
-		buffer[i] = '\0';
+	if (mpz_divisible_ui_p(n, 2U))
+		return;
+
+	mpz_init(r);
+	mpz_sqrt(r, n);
+	mpz_set_ui(*p, 3U);
+	mpz_sqrt(r, n);
+
+	while (!mpz_divisible_p(n, *p) && mpz_cmp(*p, r) < 0)
+		mpz_add_ui(*p, *p, 2U);
+		
+	if (!mpz_divisible_p(n, *p))
+		mpz_set(*p, n);
+
+	mpz_clear(r);
 }
 
 /**
- * file2buffer - Reads argv[1] to buffer
- *
- * @filename: Pointer to file location
- *
- * @buffer: Array that holds the file data
- *
- * Return: None (void function)
- */
-void file2buffer(char *filename, char *buffer)
+  * main - Entry
+  * @ac: argument count
+  * @av: string vector
+  *
+  * Return: integer
+  */
+int main(int ac, char **av)
 {
-	int opens = 0;
+	FILE *fp = NULL;
+	char *line = NULL;
+	size_t line_size = 0;
+	ssize_t line_length = -1;
+	mpz_t n, p, q, r;
 
-	opens = open(filename, O_RDONLY);
-	if (opens < 0)
+	mpz_init(n);
+	mpz_init(p);
+	mpz_init(q);
+	mpz_init(r);
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Error: usage: factors <file>\n");
 		exit(EXIT_FAILURE);
+	}
 
-	if (read(opens, buffer, S_BUFFER) < 0)
+	fp = fopen(av[1], "r");
+	
+	if (!fp)
 	{
-		close(opens);
+		fprintf(stderr, "Error: cannot open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	close(opens);
-}
 
-/**
- * _atoi - ASCII -> int conversion
- *
- * @src: Input char array
- *
- * @dest: Converted data array
- *
- * Return: Number of elements copied
- */
-long long unsigned int _atoi(char *src, long long unsigned int *dest)
-{
-	int i = 0;
-
-	for (i = 0; src[i] && src[i] != '\n'; i++)
-		dest[i] = src[i] - '0';
-
-	return (i);
-}
-
-/**
- * _putint - General agnostic number printing function
- *
- * @dest: Array of newly converted data
- *
- * @n: Size of array (N is also the principle number in PRho)
- *
- * Return: None (void function)
- */
-void _putint(long long unsigned int *dest, long long unsigned int n)
-{
-	int i = 0;
-
-	for (i = 0; i < n; i++)
-		putchar(dest[i] + '0');
-}
-
-/**
- * compute - Converts int array to int
- *
- * @src: Array of integers forming number
- *
- * @n: Number of digits in number
- *
- * Return: Integer repr. of array of ints
- */
-long long unsigned int compute(long long unsigned int *src, long long unsigned int n)
-{
-        int i;
-	long long unsigned int result = 0;
-
-        for (i = 0; i < n; i++)
+	while ((line_length = getline(&line, &line_size, fp)) != -1)
 	{
-                result *= 10;
-		result += src[i];
+		line[line_length - 1] = '\0';
+		
+		mpz_set_str(n, line, 10);
+
+		factor(&p, n);
+		mpz_tdiv_q(q, n, p);
+
+		mpz_out_str(stdout, 10, n);
+		printf("=");
+		mpz_out_str(stdout, 10, q);
+		printf("*");
+		mpz_out_str(stdout, 10, p);
+		printf("\n");
 	}
 
-	//printf("result is: %llu\n", result);
-	return result;
-}
+	free(line);
+	fclose(fp);
 
-/**
- * main - Entry point
- *
- * @argc: Size of argv array
- *
- * @argv: Array of CLI inputs
- *
- * Return: always 0
- */
-int main(int argc, char **argv)
-{
-	char buffer[S_BUFFER]; /* original buffer */
-	char *token; /* tokenized buffer */
-
-	long long unsigned int number[VALUE]; /* integer repr. of number */
-	long long unsigned int cv = 1;
-
-	long long unsigned int n; /* final value to compute */
-	long long unsigned int big = 1;
-
-	if (argc != 2)
-		return (EXIT_FAILURE);
-
-	init(buffer);
-	file2buffer(argv[1], buffer);
-	token = strtok(buffer, " \n"); /* separates each number */
-
-	while (token != NULL)
-	{
-		cv = _atoi(token, number); /* converted values */
-
-		n = compute(number, cv);
-		big = n / PollardRho(n);
-		printf("%llu=%llu*%llu\n", n, big, PollardRho(n));
-
-		//_putint(number, cv);
-		//putchar('\n');
-		token = strtok(NULL, " \n"); /* breaks up each number */
-	}
+	mpz_clear(n);
+	mpz_clear(p);
+	mpz_clear(q);
+	mpz_clear(r);
 
 	return (0);
 }
